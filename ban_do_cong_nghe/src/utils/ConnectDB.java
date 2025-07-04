f360ab0b198dacf702ff775ee83fd398c68a2fd8
MinhHoangCoder <hoangmaihaminh@gmail.com>
2025-07-04 14:40:52 +0700

    push

---
+++ ban_do_cong_nghe/src/utils/ConnectDB.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.sql.*;
+
+/**
+ *
+ * @author luand
+ */
+public class ConnectDB {
+    public static Connection con = null;
+    public static Connection getConnect() {
+        String strDbUrl = "jdbc:sqlserver://localhost:1433; databaseName=OnTapJava2;user=sa;password=1234;"
+                + "encrypt=true;trustServerCertificate=true";
+        try {
+            con = DriverManager.getConnection(strDbUrl);
+            System.out.println("Ket Noi Thanh Cong");
+        } catch (SQLException e) {
+            System.out.println("Connect Loi: " + e);
+        }
+        return con;
+    }
+    public static void main(String[] args) {
+        ConnectDB.getConnect();
+    }
+}
