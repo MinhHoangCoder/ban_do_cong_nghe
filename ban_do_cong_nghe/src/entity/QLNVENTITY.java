8ba111d8265020850457b86685a5bc55c085bbe0
MinhHoangCoder <hoangmaihaminh@gmail.com>
2025-07-09 17:23:43 +0700

    build QLNV

--- ban_do_cong_nghe/src/entity/QLNVENTITY.java
+++ ban_do_cong_nghe/src/entity/QLNVENTITY.java
@@ -10,11 +10,96 @@ import java.time.LocalDate;
  * @author MSI USER
  */
 public class QLNVENTITY {
-    private int maNV, quyen;
-    private String tenNV, soDienThoai, email, diaChi, matKhau;
+    private int maNV;
+    private String tenNV, soDienThoai, email, diaChi, matKhau, quyen;
     private LocalDate ngaySinh;
 
     public QLNVENTITY() {
+    }
+
+    public QLNVENTITY(String quyen, String tenNV, String soDienThoai, String email, String diaChi, String matKhau, LocalDate ngaySinh) {
+        this.quyen = quyen;
+        this.tenNV = tenNV;
+        this.soDienThoai = soDienThoai;
+        this.email = email;
+        this.diaChi = diaChi;
+        this.matKhau = matKhau;
+        this.ngaySinh = ngaySinh;
+    }
+
+    public QLNVENTITY(int maNV, String quyen, String tenNV, String soDienThoai, String email, String diaChi, String matKhau, LocalDate ngaySinh) {
+        this.maNV = maNV;
+        this.quyen = quyen;
+        this.tenNV = tenNV;
+        this.soDienThoai = soDienThoai;
+        this.email = email;
+        this.diaChi = diaChi;
+        this.matKhau = matKhau;
+        this.ngaySinh = ngaySinh;
+    }
+
+    public int getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(int maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getQuyen() {
+        return quyen;
+    }
+
+    public void setQuyen(String quyen) {
+        this.quyen = quyen;
+    }
+
+    public String getTenNV() {
+        return tenNV;
+    }
+
+    public void setTenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public LocalDate getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(LocalDate ngaySinh) {
+        this.ngaySinh = ngaySinh;
     }
     
     
